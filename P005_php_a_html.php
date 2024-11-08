1cfcfe59ba719b444f4e25874de54b2f51ee488c
DobryDzus <77481178+DobryDzus@users.noreply.github.com>
2024-11-08 10:29:36 +0100

    Add files via upload

---
+++ P005_php_a_html.php
@@ -0,0 +1,33 @@
+<?php
+
+//tady pisu kdyz chci zpracovat kod pri spusteni stranky - login, omezeni, redirect
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>ukazka pripojeni php k html</h1>
+
+    <h2>
+    <?php
+        echo "ahoj";
+    //tady kdyz chci zpracovat kod ktery ma byt na strance videt nebo je aktivne zavisly na body html - prvky, data, vypisy, interaktivita
+    
+    ?>
+    </h2>
+    
+</body>
+</html>
+<?php
+
+//tady kdyz chci zpracovat kod po spusteni stranky - odeslani frm, zpracovani dat, double check, redirect
+
+?>
