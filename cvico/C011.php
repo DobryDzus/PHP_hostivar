7f058af0847f555c7d9cc1dd354c42c542b6ed30
DobryDzus <77481178+DobryDzus@users.noreply.github.com>
2024-11-08 10:37:08 +0100

    Add files via upload

---
+++ cvico/C011.php
@@ -0,0 +1,26 @@
+<?php
+$jmeno = "patrik";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "vitejte na nasi webove strance!";
+        ?>
+    </h1>
+
+    <h2>
+        <?php
+        echo "ahoj, jmenuju se ".$jmeno;
+        ?>
+    </h2>
+</body>
+</html>
