1c09a2d9081eda629a7cae55f90e1bd0b8387372
DobryDzus <77481178+DobryDzus@users.noreply.github.com>
2024-11-14 10:28:04 +0100

    Add files via upload

---
+++ P009-Require_include.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>tady je nize umisten obsah souboru P009-test_i.html</h1>
+    <?php
+        include "P009-test_i.html"
+    ?>
+
+
+    <h1>tady je nize umisten obsah souboru P009-test_r.php</h1>
+    <?php
+        require "P009-test_r.php"
+    ?>
+
+
+</body>
+</html>
---
+++ P009-test_r.php
@@ -0,0 +1,9 @@
+<?php
+$prvni = 15;
+$druha = 30;
+
+$vysledek = $prvni + $druha;
+
+echo "cauu ". $vysledek;
+
+?>
