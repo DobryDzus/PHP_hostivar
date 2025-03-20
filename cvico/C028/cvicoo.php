4779324adf6dbbac957c35bc30b91c90d7d99db3
DobryDzus <77481178+DobryDzus@users.noreply.github.com>
2025-03-20 10:04:25 +0100

    Add files via upload

---
+++ cvico/C028/cvicoo.php
@@ -0,0 +1,23 @@
+<?php
+$a = 2;
+while ($a <= 20) {
+    echo $a;
+    $a +=2;
+}
+
+$b = 1;
+while ($b <= 10) {
+    $soucet += $b;
+    $b+=1;
+}
+echo "<br>". $soucet;
+
+$c = 1;
+
+echo "<table>";
+do {
+    echo "<tr><td>7</td>". (7*$c). "</td>";
+    $nasobek +=1;
+} while ($c <= 10);
+
+?>
